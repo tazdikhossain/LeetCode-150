98b22d2c79c9d736bfdeeb31f1a25f81ff1533b2
TAZDIK HOSSAIN <tazdik755@gmail.com>
2024-11-27 23:35:10 +0600

    Create Word Pattern.cpp

---
+++ Word Pattern.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        
+        if (pattern.size() != words.size()) {
+            return false;
+        }
+        
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        
+        for (int i = 0; i < pattern.size(); ++i) {
+            char ch = pattern[i];
+            string w = words[i];
+            
+            if (charToWord.count(ch)) {
+                if (charToWord[ch] != w) {
+                    return false;
+                }
+            } else {
+                charToWord[ch] = w;
+            }
+            
+            if (wordToChar.count(w)) {
+                if (wordToChar[w] != ch) {
+                    return false;
+                }
+            } else {
+                wordToChar[w] = ch;
+            }
+        }
+        return true;
+    }
+};
