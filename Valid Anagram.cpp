c30d020bb5c74e8e6d3ae30a742fdc623931da7b
TAZDIK HOSSAIN <tazdik755@gmail.com>
2024-11-28 23:30:47 +0600

    Create Valid Anagram.cpp

---
+++ Valid Anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        vector<int> count(26, 0); 
+        
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+        for (char c : t) {
+            count[c - 'a']--;
+        }
+        
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
