e74b39a526259a1fc729b04ae976a491e83488dc
TAZDIK HOSSAIN <tazdik755@gmail.com>
2025-09-05 23:38:10 +0600

    Populating Next Right Pointers in Each Node II.cpp

---
+++ Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return nullptr;
+
+        Node* curr = root; // current node at current level
+
+        while (curr) {
+            Node dummy(0); // dummy node for the next level
+            Node* tail = &dummy; // tail of the next level list
+
+            // iterate over current level
+            while (curr) {
+                if (curr->left) {
+                    tail->next = curr->left;
+                    tail = tail->next;
+                }
+                if (curr->right) {
+                    tail->next = curr->right;
+                    tail = tail->next;
+                }
+                curr = curr->next;
+            }
+
+            // move to the first node of the next level
+            curr = dummy.next;
+        }
+
+        return root;
+    }
+};
